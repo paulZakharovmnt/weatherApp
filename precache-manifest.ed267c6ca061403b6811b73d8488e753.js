bc3aee4f1c3fdc78ebdaded3e21b2e8362c9a6cc
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-09-15 09:41:13 -0400

    Updates

---
+++ precache-manifest.ed267c6ca061403b6811b73d8488e753.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913447231f60da0d7126b5047f21470c",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "053e68e51035c69f3c44",
+    "url": "/weatherApp/static/css/main.2ef6d6bf.chunk.css"
+  },
+  {
+    "revision": "ae79f5c41b4a01e4c365",
+    "url": "/weatherApp/static/js/2.81cc8994.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.81cc8994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053e68e51035c69f3c44",
+    "url": "/weatherApp/static/js/main.403a600b.chunk.js"
+  },
+  {
+    "revision": "49a43c9aa506d01919bb",
+    "url": "/weatherApp/static/js/runtime-main.56e0137e.js"
+  },
+  {
+    "revision": "63d630801a92337d1843ed24a2a4badb",
+    "url": "/weatherApp/static/media/backgrnd.63d63080.jpg"
+  },
+  {
+    "revision": "7162014c055e6a114eb2e594335fa45c",
+    "url": "/weatherApp/static/media/day-bg.7162014c.jpg"
+  },
+  {
+    "revision": "59284836e527acfc51de3bd07cf0a73a",
+    "url": "/weatherApp/static/media/evening-bg.59284836.jpg"
+  },
+  {
+    "revision": "89d4cbd8fc07a4e83a4f5dd30b7a9b44",
+    "url": "/weatherApp/static/media/morning-bg.89d4cbd8.jpg"
+  },
+  {
+    "revision": "1633e1d3cdc7b62257a5c7e3d7ad440b",
+    "url": "/weatherApp/static/media/night-bg.1633e1d3.jpg"
+  }
+]);
