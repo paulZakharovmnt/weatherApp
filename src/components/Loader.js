cd3b489c3dac48ae507b686450b7741380c757bd
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-01 15:17:30 -0500

    Add spinner loader

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loading-page">
+      <div className="spinner">
+        <span className="loader"></span>
+        Loading...
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
